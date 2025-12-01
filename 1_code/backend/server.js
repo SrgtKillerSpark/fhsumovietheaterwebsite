55f3e1326b514f2eb26f5cce0056d81794639fcc
rasmith26 <rasmith26@mail.fhsu.edu>
2025-12-01 11:43:34 -0600

    add: added databsase.js that connects to mySQL database can query and insert data.

--- 1_code/backend/server.js
+++ 1_code/backend/server.js
@@ -1,10 +1,15 @@
 import express from 'express';
+import db from './database/database.js';
 
 const app = express();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5500;
 
 app.listen(PORT, () => {
     console.log(`Running on port ${PORT}`);
 });
 
+app.get('/', async (req, res) => {
+    const data = await db.query('SELECT name, seats, type FROM theater WHERE theater_id = ?', [1]);
+    res.send(data[0]);
+});
